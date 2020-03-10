d70626074598988f1ce51fbe690e3921fd7926c2
Ketan <ketantoo@gmail.com>
2020-03-10 12:38:25 +0530

    Directory, MenuItem component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Homepage from "./Homepage.component";
+import Homepage from "./pages/Homepage.component";
 
 function App() {
   return (
