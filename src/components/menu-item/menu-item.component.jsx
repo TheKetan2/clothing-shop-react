8e77ede517ff226e954deeff61fb11098d8fe0e8
Ketan <ketantoo@gmail.com>
2020-03-10 13:27:27 +0530

    Directory, MenuItem component styling finished

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,10 +4,11 @@ import "./menu-item.styles.scss";
 
 export default function MenuItem({ title, imageUrl, size }) {
   return (
-    <div
-      className={`menu-item ${size}`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`menu-item ${size}`}>
+      <div
+        className="backgrond-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
