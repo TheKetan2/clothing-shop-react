b35520274b593106feca5b712e14e3ef639f8773
Ketan <ketantoo@gmail.com>
2020-03-10 11:22:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Homepage from "./Homepage.component";
+
+function App() {
+  return (
+    <div className="">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
