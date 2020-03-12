33dfa3f5832cf6925248b7bd2d50f7565359e962
Ketan <ketantoo@gmail.com>
2020-03-12 10:41:45 +0530

    CollectionPreview and CollectionItem componens created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="">
-      <Route exact path="/" component={Homepage} />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
