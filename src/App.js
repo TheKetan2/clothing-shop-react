86272723b24fb10c8ebaac80de5cb670235b82e9
Ketan <ketantoo@gmail.com>
2020-03-17 12:49:50 +0530

    Implementing Firebase auth

--- src/App.js
+++ src/App.js
@@ -5,23 +5,43 @@ import Homepage from "./pages/Homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div className="">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component() {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      console.log(user);
+      this.setState({
+        currentUser: user
+      });
+    });
+  }
+
+  componentWillMount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
